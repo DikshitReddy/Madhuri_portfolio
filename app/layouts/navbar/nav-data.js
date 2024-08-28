67de640ee2f86d642de479b9f1b7c461c4ecb1ff
Reddy V R <dikshit.reddyvr@unisys.com>
2024-08-28 23:09:46 +0530

    Initial commit

---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,38 @@
+import config from '~/config.json';
+
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Artistry',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/${config.twitter}`,
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: `https://www.figma.com/${config.figma}`,
+    icon: 'figma',
+  },
+  {
+    label: 'Behance',
+    url: `https://www.behance.net/madhurireddy7`,
+    icon: 'behance',
+  },
+];
---
+++ app/routes/projects.slice/slice.jsx
@@ -0,0 +1,195 @@
+import sliceAnnotationLarge from '~/assets/slice-annotation-large.png';
+import sliceAnnotationPlaceholder from '~/assets/slice-annotation-placeholder.png';
+import sliceAnnotation from '~/assets/slice-annotation.png';
+import sliceAppLarge from '~/assets/slice-app-large.jpg';
+import sliceAppPlaceholder from '~/assets/slice-app-placeholder.jpg';
+import sliceApp from '~/assets/slice-app.jpg';
+import sliceBackgroundBarLarge from '~/assets/slice-background-bar-large.jpg';
+import sliceBackgroundBarPlaceholder from '~/assets/slice-background-bar-placeholder.jpg';
+import sliceBackgroundBar from '~/assets/slice-background-bar.jpg';
+import sliceBackgroundLarge from '~/assets/slice-background-large.jpg';
+import sliceBackgroundPlaceholder from '~/assets/slice-background-placeholder.jpg';
+import sliceBackground from '~/assets/slice-background.jpg';
+import sliceIrlPlaceholder from '~/assets/slice-irl-placeholder.jpg';
+import sliceIrl from '~/assets/slice-irl.jpg';
+import sliceSidebarAnnotationsLarge from '~/assets/slice-sidebar-annotations-large.png';
+import sliceSidebarAnnotationsPlaceholder from '~/assets/slice-sidebar-annotations-placeholder.png';
+import sliceSidebarAnnotations from '~/assets/slice-sidebar-annotations.png';
+import sliceSidebarLayersLarge from '~/assets/slice-sidebar-layers-large.png';
+import sliceSidebarLayersPlaceholder from '~/assets/slice-sidebar-layers-placeholder.png';
+import sliceSidebarLayers from '~/assets/slice-sidebar-layers.png';
+import sliceSlidesLarge from '~/assets/slice-slides-large.jpg';
+import sliceSlidesPlaceholder from '~/assets/slice-slides-placeholder.jpg';
+import sliceSlides from '~/assets/slice-slides.jpg';
+import { Footer } from '~/components/footer';
+import { Image } from '~/components/image';
+import {
+  ProjectBackground,
+  ProjectContainer,
+  ProjectHeader,
+  ProjectImage,
+  ProjectSection,
+  ProjectSectionColumns,
+  ProjectSectionContent,
+  ProjectSectionHeading,
+  ProjectSectionText,
+  ProjectTextRow,
+} from '~/layouts/project';
+import { Fragment } from 'react';
+import { media } from '~/utils/style';
+import { baseMeta } from '~/utils/meta';
+import styles from './slice.module.css';
+
+const title = 'Molly Coddle';
+const description =
+  'This project is a 24/7 dependable service to serve pet owners Veterinarians.The system includes a fleet of ambulances staffed with licensed veterinary professionals andequipped with medical equipment to respond to emergencies quickly.';
+const roles = ['User Research', 'UX Design', 'Interface Design'];
+
+export const meta = () => {
+  return baseMeta({ title, description, prefix: 'Projects' });
+};
+
+export const Slice = () => {
+  return (
+    <Fragment>
+      <ProjectContainer className={styles.slice}>
+        <ProjectBackground
+          src={sliceBackground}
+          srcSet={`${sliceBackground} 1280w, ${sliceBackgroundLarge} 2560w`}
+          width={1280}
+          height={800}
+          placeholder={sliceBackgroundPlaceholder}
+          opacity={0.8}
+        />
+        <ProjectHeader
+          title={title}
+          description={description}
+          url="https://www.figma.com/design/o0crCYcEPQo5DUeg79VVJ1/MollyCoddle?node-id=1-8654&t=diJE1PMW9XOWIlqo-0"
+          roles={roles}
+        />
+        <ProjectSection padding="top">
+          <ProjectSectionContent>
+            <ProjectImage
+              srcSet={`${sliceApp} 800w, ${sliceAppLarge} 1920w`}
+              width={800}
+              height={500}
+              placeholder={sliceAppPlaceholder}
+              alt="The Slice web application showing a selected user annotation."
+              sizes={`(max-width: ${media.mobile}px) 100vw, (max-width: ${media.tablet}px) 90vw, 80vw`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionColumns centered className={styles.columns}>
+            <div className={styles.imagesText}>
+              <ProjectSectionHeading>Bringing it together</ProjectSectionHeading>
+              <ProjectSectionText>
+                Teachers needed a better way to create collaborative group activities by
+                annotating slides on Slice. Before starting this project, a layer could
+                only be annotated by a single user, making it difficult for learners to
+                work together.
+              </ProjectSectionText>
+              <ProjectSectionText>
+                Our solution was to provide users with a mobile application that enables
+                to access our services whenever you choose.
+              </ProjectSectionText>
+            </div>
+            <div className={styles.sidebarImages}>
+              <Image
+                className={styles.sidebarImage}
+                srcSet={`${sliceSidebarLayers} 350w, ${sliceSidebarLayersLarge} 700w`}
+                width={350}
+                height={750}
+                placeholder={sliceSidebarLayersPlaceholder}
+                alt="The layers sidebar design, now with user profiles."
+                sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
+              />
+              <Image
+                className={styles.sidebarImage}
+                srcSet={`${sliceSidebarAnnotations} 350w, ${sliceSidebarAnnotationsLarge} 700w`}
+                width={350}
+                height={750}
+                placeholder={sliceSidebarAnnotationsPlaceholder}
+                alt="Multiple user annotations on a shared layer."
+                sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
+              />
+            </div>
+          </ProjectSectionColumns>
+        </ProjectSection>
+        <ProjectSection light>
+          <ProjectSectionContent>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Improving the experience</ProjectSectionHeading>
+              <ProjectSectionText>
+                The experts in the field of veterinary health care will benefit from the planned service.
+                Centralized databse that holds infomation of pets. the service will give the veterinarian access to the pet's
+                information in the databse, allowing them to diagnose their medical state and formulate the necessary therapy.
+                MCVC is seen as a useful service that serves time and money on healthcare.
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              srcSet={`${sliceSlides} 800w, ${sliceSlidesLarge} 1920w`}
+              width={800}
+              height={500}
+              placeholder={sliceSlidesPlaceholder}
+              alt="The new My Slides tab in slice, showing annotated and favorited slides."
+              sizes={`(max-width: ${media.mobile}px) 500px, (max-width: ${media.tablet}px) 800px, 1000px`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection padding="top">
+          <ProjectSectionContent className={styles.grid}>
+            <div className={styles.gridImage}>
+              <div className={styles.gridBackground}>
+                <Image
+                  srcSet={`${sliceBackgroundBar} 440w, ${sliceBackgroundBarLarge} 880w`}
+                  width={40}
+                  height={90}
+                  placeholder={sliceBackgroundBarPlaceholder}
+                  alt=""
+                  role="presentation"
+                  sizes={`(max-width: ${media.mobile}px) 312px, (max-width: ${media.tablet}px) 408px, 514px`}
+                />
+              </div>
+            </div>
+            <div className={styles.gridText}>
+              <ProjectSectionHeading>Trusted Service</ProjectSectionHeading>
+              <ProjectSectionText>
+                Your Trusted Partner in Pet care, Offering Tailored Services
+                to Ensure the Health, Happiness, and Well-Being of Your Beloved
+                Furry Companions.Additionally, the system incorporates advanced 
+                medical record-keeping technologies to maintain accurate and up-to-date 
+                medical histories for each pet, allowing for more effective and 
+                personalized care.Overall, the system aims to improve the quality 
+                and accessibility of veterinary care for pets.
+              </ProjectSectionText>
+            </div>
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectSectionContent>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Project outcomes</ProjectSectionHeading>
+              <ProjectSectionText>
+              Project aims to enhance pet health management by providing a seamless 
+              platform for veterinary services. It enables pet owners to schedule 
+              appointments, access medical records, and consult with veterinarians 
+              remotely, improving the accessibility and convenience of pet care. 
+              The application supports veterinarians with efficient patient management 
+              tools, leading to better time management and service delivery.
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              src={sliceIrl}
+              width={940}
+              height={500}
+              placeholder={sliceIrlPlaceholder}
+              alt="Students at the University of New South Wales using the new collaborative annotation features"
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+      </ProjectContainer>
+      <Footer />
+    </Fragment>
+  );
+};
